09adf3b3d989e237535ec8711e57e87d2ef50ed1
yangyingchao <yangyingchao@gmail.com>
2013-05-25 21:51:50 +0800

    auto.

--- .fvwm/tools/menu_gen.py
+++ .fvwm/tools/menu_gen.py
@@ -21,246 +21,264 @@ fvwm_menu_output = os.path.join(FVWM_HOME, "Menu")
 
 DESKTOP_SEARCH_PATH = ["/usr/share/applications",
                        os.path.join(USER_HOME, ".local/share/applications")]
-ICON_PATHS          = ["/usr/share/pixmaps", "/usr/share/icons/hicolor",
-                       os.path.join(USER_HOME, ".icons/default")]
 
-#             +-----------+------------+-------------+-----------+
-# fvwm_menu = | Category1 | Category2  | Category 3  | ........  |
-#             +----+------+------------+-------------+-----------+
-#                  |
-#                  v                 +--> Name = NAME1
-#             +-----------+          |
-#             |   NAME 1  |--------> +--> Exec = ...
-#             +-----------+          |
-#             |  Entry 2  |          +--> Icon = ...
-#             +-----------+
-#             |  Entry 3  |
-#             +-----------+
-#             |    .      |
-#             +-----------+
+class ImageCollection:
+    def __init__(self, paths):
+        self.img_data = {}
+        for path in paths:
+            if os.path.islink(path):
+                path = os.path.abspath(path)
+                os.path.walk(path, self._collect, None)
+        print "Finished icon collection, size: ", len(self.img_data)
 
-fvwm_menu     = {}
-img_data      = {}
-CATEGORY_LIST = ["Office", "Graphics", "System", "Engineering" "Utility",
-                 "Network", "Development", "AudioVideo"]
-keywords      = ["Name", "Exec", "Icon", "Categories"]
+    def PrepareIcon(self, name, isCat=False):
+        icon_fallback = ""
+        if os.access(name, os.F_OK): # Input is the absloute path of file.
+            icon_fallback = name;
+        if isCat: # Categories start with applications-.
+            name = name.lower()
+            if "network" in name:
+                name = "internet.png"
+            elif "audiovideo" in name:
+                name = "multimedia.png"
+            name = "applications-%s"%name
 
-verbose = True
+        bname = os.path.basename(name)
+        icon_path = ""
+        pos = bname.rfind(".")
+        if pos != -1:
+            bname = bname[:pos]
+        icon_path = self.img_data.get(bname.lower())
 
-def log_print(msg):
-    global verbose
-    if verbose:
-        print(msg)
+        if icon_path is None:
+            icon_path = icon_fallback
+        icon_out = ""
+        if icon_path:
+            icon_out = os.path.join(fvwm_icon_home, name)
+            os.system('convert -background none -resize 24x24 "%s" "%s"'%(
+                icon_path, icon_out))
+
+        return icon_out
+
+    def _collect(self, arg, dirname, filenames):
+        #TODO: This should be optimized by analyzing "index.theme" file.
+        size = len(filenames)
+        for i in range(size):
+            idx = size - 1 -i;
+            fn = filenames[idx]
+            if fn.find("16") != -1 or fn.find("22") != -1 or \
+               fn.find("32") != -1 or fn.find("24") != -1:
+                filenames.pop(idx);
+                continue
+
+            path = os.path.join(dirname, fn)
+            if os.path.isdir(path):
+                continue
+
+            key = fn.split(".")[0].lower()
+            self.img_data[key] = path
+
+g_iconBase = ImageCollection(["/usr/share/pixmaps", "/usr/share/icons/hicolor",
+                              os.path.join(USER_HOME, ".icons/default")])
+
+def SimpleRead(fn):
+    """
+    read file and return content.
+    Arguments:
+    - `fn`:
+    """
+    content = ""
+    try:
+        content = open(fn).read()
+    except :
+        print("Failed to read file: %s\n"%(fn))
+        print sys.exc_info()[1]
+
+    return content
+
+
+def SimpleWrite(fn, content):
+    """
+
+    Arguments:
+    - `fn`:
+    - `content`:
+    """
+    fd = None
+    try:
+        fd = open(fn, "w")
+    except:
+        print("Failed to open file: %s for writting\n"%(fn))
+        print sys.exc_info()[1]
+        return False
     else:
+        fd.truncate(0)
+        fd.write(content)
+        fd.close()
+        return True
+
+def remove_if(lst, ele):
+    while ele in lst:
+        lst.remove(ele)
+
+class DesktopEntry:
+    """
+    DesktopEntry representation.
+    """
+
+    def __init__(self, path):
+        """
+        """
+        self.Category = None
+        self.Name     = None
+        self.Icon     = None
+        self.Exec     = None
+
+        # Optional
+        self.terminal = False
+
+        self.IsValid = True
+
+        self._parse(path)
+
+    def _parse(self, path):
+        """
+        """
+        content = SimpleRead(path)
+        if "Desktop Entry" not in content:
+            self.IsValid = False
+            return
+
+        content = content.split("\n")
+        tmp_dic = {}
+        for item in content:
+            kvp = item.split("=")
+            if len(kvp) == 2:
+                tmp_dic[kvp[0]] = kvp[1]
+
+        self.Icon = tmp_dic.get("Icon")
+        self.Name = tmp_dic.get("Name")
+        self.Exec = tmp_dic.get("Exec")
+
+        if self.Name is None or self.Exec is None:
+            self.IsValid = False
+            return
+
+        self.Exec = self.Exec[:self.Exec.rfind("%")]
+
+        # Update self.Category if necessary
+        self.Category = tmp_dic.get("Categories")
+        self._updateCategory(tmp_dic.get("MimeType"))
+
+    def _updateCategory(self, mimes):
+        if self.Category is None:
+            self.Category = "Other"
+
+        if (mimes is not None) and ("audio" in mimes or "video" in mimes):
+            self.Category = "AudioVideo"
+        elif "Windows" in self.Name or "Microsoft" in self.Name or \
+             "cxoffice" in self.Name or "cxmenu" in self.Category or \
+             "cross"    in self.Name.lower():
+            self.Category = "Wine"
+        elif "Develop" in self.Category:
+            self.Category = "Development"
+        else:
+            self.Category = self.Category.split(";")[0]
+
+
+    def SerializeToString(self):
+        """
+        It will do two things:
+        1. Find icon and convert to proper size.
+        2. Return a string which can be inserted into fvwm menu.
+        """
+
+        icon_out = g_iconBase.PrepareIcon(self.Icon)
+        return '+ "%%%s%%%s" Exec exec %s\n'%(icon_out, self.Name,
+                                              self.Exec)
+
+class DE_Category:
+    """
+    """
+    def __init__(self, name):
+        """
+        """
+        self.Name = name
+        self.Icon = name + ".png"
+        self.DEs=set()
+        print "Createing CAT: ", name
         pass
 
-def gen_img_data():
-    for path in ICON_PATHS:
-        if os.path.islink(path):
-            path = os.path.realpath(path)
-        print("\t Processing icon directory: %s"%path)
-        walk(path, process_img, None)
+    def StoreEntry(self, de):
+        self.DEs.add(de)
 
-def process_img(arg, dirname, filenames):
-    """
-    Process each image in subdir, store in img_data.
-    """
-    for fn  in filenames:
-        path = os.path.join(dirname, fn)
-        if "16x16" in path or "22x22" in path: # Skip: icon is too small.
-            continue
-        if os.path.isdir(path):
-            continue
-        else:
-            key = os.path.basename(path).split(".")[0].lower()
-            img_data[key] = path
+    def SerializeToString(self):
+        """ Returns a tuple.
+        First part is the entry to be added to MenuFvwmRoot.
+        Second part is menu definition of this category.
+        """
+        print "Called for: ", self.Name, "Len: ", len(self.DEs)
+        icon_out = g_iconBase.PrepareIcon(self.Icon, True)
+        first = '+ "%%%s%%%s" Popup Menu%s\n'%(icon_out, self.Name, self.Name)
+        second = "\nDestroyMenu Menu%s\nAddToMenu Menu%s\n"%(self.Name, self.Name)
+        for de in self.DEs:
+            second += de.SerializeToString()
+        second += "\n\n"
+        return (first, second)
 
-def process_desktop_entries(arg, dirname, filenames):
+class FvwmMenuFactory:
+    def __init__(self):
+        self.cats = {}
+        pass
+
+    def Feed(self, path):
+        """Feed and parse path, generate DE and store it.
+
+        Arguments:
+        - `path`:
+        """
+        de = DesktopEntry(path)
+        if not de.IsValid:
+            print("File %s path is not valid!\n"%path)
+            return
+
+        cat = self.cats.get(de.Category)
+        if cat is None:
+            cat = DE_Category(de.Category)
+            self.cats[de.Category] = cat
+        self.cats[de.Category].StoreEntry(de)
+
+    def OutputToFile(self, of=os.path.join(FVWM_HOME, "Menu")):
+        output   = SimpleRead(menu_template_head)
+        submenus = []
+        for cat in self.cats.values():
+            content = cat.SerializeToString()
+            output += content[0]
+            submenus.append(content[1])
+
+        output += SimpleRead(menu_template_tail)
+
+        for sm in submenus:
+            output += sm + "\n"
+        SimpleWrite(of, output)
+
+def process_desktop_entries(menu, dirname, filenames):
     """
     Parse each file in this subdir, store information into fvwm_menu;
     """
     for filename in filenames:
         path = os.path.join(dirname, filename)
-        print (("Processing %s"%path))
-        if os.path.isdir(path):
+        if os.path.isdir(path) or not path.endswith("desktop"):
             continue
         else:
-            tmp = parse_single(path)
-            if tmp.get("Show") :
-                add_to_menubase(deepcopy(tmp))
-
-def parse_single(path):
-    """
-    Parse single desktop entry, return the pared entry.
-    Arguments:
-    - `path`:
-    """
-    try:
-        content = open(path).readlines()
-    except:
-        print("Exception when proceesing file:%s", path)
-        return
-
-    skip = False # Special items that we don't want to display, such as Games.
-    tmp_dic = {}
-    for item in content:
-        for key in keywords:
-            if item.startswith(key+"="):
-                val = item.split("=")[-1].strip("\n")
-                if key == "Categories": # Catergories
-                    pos = val.find(";")
-                    if  pos != -1:
-                        vlist = val.split(";")
-                        val = "Other"
-                        for v in vlist:
-                            if v.find("Game") != -1:
-                                skip = True
-                                print("Skip item.")
-                                break;
-                            if v in CATEGORY_LIST:
-                                val = v
-                                break
-                elif key == "Exec":
-                    pos = val.rfind("%")
-                    if pos != -1:
-                        val = val[:pos]
-                    pos = val.rfind(" -")
-                    if pos != -1:
-                        val = val[:pos]
-                tmp_dic[key] = val
-                break
-    if tmp_dic.get("Icon") is None or "@" in tmp_dic.get("Icon") or skip:
-        tmp_dic["Show"] = False
-    else:
-        tmp_dic["Show"] = True
-
-    return deepcopy(tmp_dic)
-
-def add_to_menubase(dic):
-    """
-    Add parsed menu into global database.
-    """
-    global fvwm_menu
-    if dic.get("Categories") is None:
-        dic["Categories"] = "Other"
-
-    if not fvwm_menu.has_key(dic["Categories"]):
-        fvwm_menu[dic["Categories"]] = {}
-    if not dic["Name"] in fvwm_menu[dic["Categories"]].keys():
-        fvwm_menu[dic["Categories"]][dic["Name"]] = dic
-
-def sort_name(entry1, entry2):
-    return (entry1["Name"] <= entry2["Name"])
-
-def find_icon(name, menu_type=1):
-    """
-    """
-    icon_fallback = ""
-    if os.access(name, os.F_OK): # Input is the absloute path of file.
-        icon_fallback = name;
-    if not menu_type: # Categories start with applications-.
-        if name == "network":
-            name = "internet"
-        elif name == "audiovideo":
-            name = "multimedia"
-        name = "applications-%s.png"%name
-        print ("Name: %s"%name)
-
-    name = os.path.basename(name)
-    pos = name.rfind(".")
-    if pos != -1:
-        name = name[:pos]
-    icon_path = img_data.get(name.lower())
-    if icon_path is None:
-        print ("Using default: %s"%icon_fallback)
-        icon_path = icon_fallback
-    return icon_path
-
+            menu.Feed(path)
 
 if __name__ == '__main__':
 
-    if len(sys.argv) > 1:
-        verbose = True
-
-    print ("Generating image database ...")
-    gen_img_data()
-    print ("Finished to generate image database\n")
+    menu = FvwmMenuFactory()
 
     print ("Searching and analyzing desktop entries...")
     for item in DESKTOP_SEARCH_PATH:
-        walk(item, process_desktop_entries, item)
+        os.path.walk(item, process_desktop_entries, menu)
     print ("Finished analyze desktop entries.\n")
-
-    print ("Writing new menu items for fvwm...")
-    content = open(menu_template_head).read() # Head of template
-    ### Generate categories.
-    try:
-        os.system("rm -rf ~/.fvwm/icons/apps/*")
-    except  :
-        print ("Execption when deleting old icons:")
-
-    for key in fvwm_menu.keys():
-        icon_path = find_icon(key.lower(), 0)
-        icon_out = ""
-
-        if len(icon_path):
-            icon_out = os.path.join(fvwm_icon_home,
-                                    os.path.basename(icon_path));
-            if not icon_out.endswith(".png"):
-                pos = icon_out.rfind(".")
-                icon_out = icon_out[:pos] + ".png"
-            os.system('convert -background none -resize 24x24 "%s" "%s"'%\
-                          (icon_path, icon_out))
-
-        content += '+ "%%%s%%%s" Popup Menu%s\n'%(icon_out, key, key)
-
-    content += open(menu_template_tail).read() # Tail fo template
-
-
-    ### Generate Desktop entry.
-
-    for key in fvwm_menu.keys():
-        content += "DestroyMenu Menu%s\nAddToMenu Menu%s\n"%(key, key)
-
-        va_list = fvwm_menu[key].keys()
-        va_list.sort()
-
-        log_print (("Adding category: %s"%key))
-        for v in va_list:
-            val = fvwm_menu[key].get(v)
-            if val is None:
-                print ("Invalid :", key, v)
-                continue
-
-            name = val.get("Name")
-            exec_file = val.get("Exec")
-            icon = val.get("Icon")
-            if name is None or exec_file is None or icon is None:
-                continue
-
-            log_print (("\t+---- Adding entry: %s"%name))
-            icon_path = find_icon(icon, 1)
-            icon = os.path.basename(icon)
-            pos = icon.rfind(".")
-            if pos != -1:
-                icon = icon[:pos]
-            icon_out = ""
-
-            if len(icon_path):
-                icon_out = os.path.join(fvwm_icon_home, "%s.png"%icon).lower();
-                os.system('convert -background none -resize 24x24 "%s" "%s"'%(icon_path, icon_out))
-
-            content += '+ "%%%s%%%s" Exec exec %s\n'%(icon_out, name,
-                                                      exec_file)
-        log_print ("\n")
-
-
-        content += "\n\n"
-
-    print ("Write to file: %s"%fvwm_menu_output)
-    fp = open(fvwm_menu_output, "w")
-    fp.write(content)
-    fp.flush()
-    fp.close()
-
-    print ("All work done!")
+    menu.OutputToFile()
